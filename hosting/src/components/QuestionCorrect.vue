8ad97a940ce6c1b81c3ba530cdaed1188832d23e
Merwin Hill <merwin@quicknet.nl>
2022-10-28 10:17:13 +0200

    Put confeti behind buttons

--- hosting/src/components/QuestionCorrect.vue
+++ hosting/src/components/QuestionCorrect.vue
@@ -45,7 +45,7 @@ setTimeout(function () {
 </script>
 
 <template>
-  <img id="confetti" src="../assets/gif_5.gif" class= />
+  <img id="confetti" src="../assets/gif_5.gif" />
   <div class="biggy">
     <span class="green">Correct!</span><br />
     Do you want to leave a new question for the next one?<br />
@@ -67,7 +67,7 @@ img {
   width: 800px;
   left: 0;
   top: 20%;
-  z-index: 1;
+  z-index: -1;
 }
 img.hidden {
   display: none;
